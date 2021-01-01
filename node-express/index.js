72de66065efc25713fb92fbefa57a41d7dbcf58f
Nitish Mittal <nitish7134@gmail.com>
2021-01-02 04:35:47 +0530

    Express Router

--- node-express/index.js
+++ node-express/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 
 const hostname = 'localhost';
@@ -8,6 +10,11 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+
+app.use('/dishes', dishRouter);
+
 app.use(express.static('./../node-http/public'));
 
 app.use((req, res, next) => {
@@ -16,6 +23,7 @@ app.use((req, res, next) => {
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
+
 
 
 const server = http.createServer(app);
