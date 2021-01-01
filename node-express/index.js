ffec99251c2e392e54fc31cc101599d758b5367a
Nitish Mittal <nitish7134@gmail.com>
2021-01-02 04:40:39 +0530

    Assignment 1

--- node-express/index.js
+++ node-express/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 
 const hostname = 'localhost';
@@ -14,7 +17,8 @@ app.use(bodyParser.json());
 
 
 app.use('/dishes', dishRouter);
-
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 app.use(express.static('./../node-http/public'));
 
 app.use((req, res, next) => {
