0f281b5af5b2064fc0265aa8d98a2b6f9c2a2314
Nitish Mittal <nitish7134@gmail.com>
2021-01-02 13:25:07 +0530

    Passport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -8,7 +8,8 @@ var FileStore = require('session-file-store')(session);
 
 const mongoose = require('mongoose');
 
-
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -44,27 +45,26 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-function auth(req, res, next) {
-  console.log(req.session);
 
-  if (!req.session.user) {
+function auth (req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+        next();
   }
 }
+
+
 
 app.use(auth);
 
